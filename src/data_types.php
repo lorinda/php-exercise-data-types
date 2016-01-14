ff0dbe7ef869ee1c9ba51cdecb07d0321870b216
Lorinda Peoples <lorinda@users.noreply.github.com>
2016-01-14 18:52:32 -0500

    Create data_types.php
    Week 1 Exercise: Data Types in PHP

---
+++ src/data_types.php
@@ -0,0 +1,23 @@
+<?php
+
+function convert_to_int($input){
+
+}
+
+function convert_to_float($input){
+}
+
+function convert_to_string($input){
+}
+
+function convert_to_bool($input){
+}
+
+function convert_to_array($input){
+}
+
+function convert_to_null($input){
+}
+
+
+?>
